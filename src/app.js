f19d8279c47e9298dd180788fe4036609e34b835
amateur-hacker <amateurhacker2004@gmail.com>
2024-05-16 04:16:41 +0530

    allowed cors origin for all

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(responseMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*",
   credentials: true,
 };
 
