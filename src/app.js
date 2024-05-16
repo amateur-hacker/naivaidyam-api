52079a577f51145228a0ba8f43ed2b3e128b451f
amateur-hacker <amateurhacker2004@gmail.com>
2024-05-17 03:20:29 +0530

    updated cors options

--- src/app.js
+++ src/app.js
@@ -39,28 +39,29 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use(function (req, res, next) {
-//   var allowedDomains = [
-//     "https://naivaidyam.vercel.app",
-//     "http://localhost:5173",
-//   ];
-//   var origin = req.headers.origin;
-//   if (allowedDomains.indexOf(origin) > -1) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
-//
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type, Accept",
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//
-//   next();
-// });
+app.use(function (req, res, next) {
+  var allowedDomains = [
+    "https://naivaidyam.vercel.app",
+    "http://localhost:5173",
+    "https://naivaidyam-api.vercel.app",
+  ];
+  var origin = req.headers.origin;
+  if (allowedDomains.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept",
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
