f9a2647f644095d88d75991df5511d08e44a4796
amateur-hacker <amateurhacker2004@gmail.com>
2024-05-16 11:41:42 +0530

    made changes in app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["https://naivaidyam.vercel.app/", "http://localhost:3000"],
+  origin: ["https://naivaidyam.vercel.app/", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
